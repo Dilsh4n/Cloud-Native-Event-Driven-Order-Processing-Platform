dd2389b11a53f05dbaf5f3d34745bb851d9acc18
dilsh4n <dilshangamage107@gmail.com>
2026-08-20 15:15:01 +0530

    feature/payment-service | added smoke test for CI

--- payment-service/src/test/java/com/orderplatform/payment_service/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/orderplatform/payment_service/PaymentServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.orderplatform.payment_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, topics = {"payment.commands","payment.events"})
 class PaymentServiceApplicationTests {
 
 	@Test
