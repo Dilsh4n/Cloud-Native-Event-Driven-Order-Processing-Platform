2d3c253d74dfa233e0b8a5372886e2f4438fa5ce
dilsh4n <dilshangamage107@gmail.com>
2026-08-20 15:09:22 +0530

    feature/order-service | added smoke test for CI

--- order-service/src/test/java/com/orderplatform/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/orderplatform/order_service/OrderServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.orderplatform.order_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, topics = {"inventory.events","payment.events", "order.events"})
 class OrderServiceApplicationTests {
 
 	@Test
