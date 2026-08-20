69e8293f00fc226abb3e8d66103dd77fd61bbaf8
dilsh4n <dilshangamage107@gmail.com>
2026-08-20 15:15:35 +0530

    feature/inventory-service | added smoke test for CI

--- inventory-service/src/test/java/com/orderplatform/inventory_service/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/orderplatform/inventory_service/InventoryServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.orderplatform.inventory_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, topics = {"inventory.commands", "inventory.events"})
 class InventoryServiceApplicationTests {
 
 	@Test
